3b9c7660adc4bb9cf60c519f6f3a3bbd84e98b7b
unknown <basdemir35@hotmail.com.tr>
2024-08-29 03:48:00 +0300

    add footer component with style

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import 'bootstrap/dist/css/bootstrap.css'
-import Navbar from './components/Navbar/Navbar'
-import IntroSection from './components/IntroSection/IntroSection'
-import Card from './components/Card/Card'
+import "bootstrap/dist/css/bootstrap.css";
+import Navbar from "./components/Navbar/Navbar";
+import IntroSection from "./components/IntroSection/IntroSection";
+import Card from "./components/Card/Card";
+import Footer from "./components/Footer/Footer";
 
 function App() {
-
   return (
     <>
-    <Navbar />
-    <IntroSection />
-    <Card />
+      <Navbar />
+      <IntroSection />
+      <Card />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
